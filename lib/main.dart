3767ac4d7b6408c9ccf8888e4f6c0afa5836d5ae
airon@tark.pro <airon@tark.pro>
2019-12-04 13:47:52 +0700

    main: init localization at the app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'localization/localization.dart';
 import 'ui/rates_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        const MyLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: supportedLocales,
       title: 'Exchange rates',
       theme: ThemeData(
         primarySwatch: Colors.teal,
